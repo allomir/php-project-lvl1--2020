af876c1af2dfff802eb5c110d08ed1efdb6d8eaa
allomir <alomir@yandex.ru>
2021-05-28 12:16:52 +0300

    sniffer CodeClimate GH-Actions

--- src/Cli.php
+++ src/Cli.php
@@ -5,14 +5,17 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcomeLine(): void {
+function welcomeLine(): void
+{
     line('Welcome to the Brain Game!');
 }
 
-function whatYourName(): string {
+function whatYourName(): string
+{
     return prompt('May I have your name?');
 }
 
-function helloLine(string $name): void {
+function helloLine(string $name): void
+{
     line("Hello, %s!", $name);
 }
